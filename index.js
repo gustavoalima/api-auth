ccbb79818081e8f041d2c08bfb3acfe8b2900ef4
gustavoalima <gustavoalima1@outlook.com>
2024-05-06 22:13:05 -0300

    mudou um monte

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app =  express();
-const bcrypt =  require('bcrypt');
+const bcrypt = require('bcrypt');
 const User = require('./models/User');
+const hashPassword = require('./middlewares/hashPassword');
 const port = 3000;
 const db = require('./db');
-
+const {generateToken} = require('./middlewares/authService');
 app.use(express.json());
 
 db.sync();
@@ -13,17 +14,33 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 
 });
+app.post('/register', 
+hashPassword ,
+async (req, res) => {
+        
+    const user = await User.create(
+        {...req.body}
 
-app.post('/register', async (req, res) => {
-    const {username, email,password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await User.create({
-        username,
-        email,
-        password: hashedPassword,
-
-    });
+    );
     res.send(user);
+
+});
+
+app.post('/login', async (req, res) => {
+    const {email, password} = req.body;
+    const user = await User.findOne({where: {email}});  
+    if (!user) {
+    return res.status(401)
+    .send('Email ou senha invalidos');
+}
+const passwordMatch =  bcrypt.
+compareSync(password, user.password);
+if(!passwordMatch) {
+    return res.status(401).send('Email ou senha invalidos');
+    }
+    const token = generateToken(user.dataValues);    
+    delete user.dataValues.password;
+res.send({user, token});
 
 });
 
